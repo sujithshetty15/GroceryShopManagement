ddd38743d262aab6e44ca2792899c13add456385
sujithshetty15 <126152726+sujithshetty15@users.noreply.github.com>
2023-06-28 21:07:18 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:groco@gmail.com">groco@gmail.com</a>
+         <a href="mailto:vs@gmail.com">vs@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Udupi, India - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Groco</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/load1.gif" alt="">
+</div>
